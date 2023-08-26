09e22f6e245b7f9c4ebd6b51303d47a4b83ee78e
longproxd <longproxd1@gmail.com>
2023-08-26 20:38:06 +0700

    update chart

--- client-react/src/components/LightWeightChart.js
+++ client-react/src/components/LightWeightChart.js
@@ -7,37 +7,10 @@ const LightWeightChart = ({ theme }) => {
     const legend = document.createElement("div");
 
     useEffect(() => {
-        const chart = createChart(chartContainerRef.current);
-
-        // const data = [
-        //     { time: "2018-12-22", value: 75.16 },
-        //     { time: "2018-12-23", value: 45.12 },
-        //     { time: "2018-12-24", value: 60.71 },
-        //     { time: "2018-12-25", value: 68.26 },
-        //     { time: "2018-12-26", value: 67.71 },
-        //     { time: "2018-12-27", value: 91.04 },
-        //     { time: "2018-12-28", value: 111.51 },
-        //     { time: "2018-12-29", value: 131.33 },
-        //     { time: "2018-12-30", value: 106.33 },
-        //     { time: "2018-12-31", value: 109.87 }
-        // ];
-
-        const data = [
-            { time: "2018-12-22", open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
-            { time: "2018-12-23", open: 45.12, high: 53.90, low: 45.12, close: 48.09 },
-            { time: "2018-12-24", open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
-            { time: "2018-12-25", open: 68.26, high: 68.26, low: 59.04, close: 60.50 },
-            { time: "2018-12-26", open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
-            { time: "2018-12-27", open: 91.04, high: 121.40, low: 82.70, close: 111.40 },
-            { time: "2018-12-28", open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
-            { time: "2018-12-29", open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
-            { time: "2018-12-30", open: 106.33, high: 110.20, low: 90.39, close: 98.10 },
-            { time: "2018-12-31", open: 109.87, high: 114.69, low: 85.66, close: 111.26 }
-        ];
-
         const color = theme === "dark" ? themeColors.darkBlue : themeColors.white;
         const textColor = theme === "dark" ? themeColors.grayishBlue : themeColors.darkBlue;
 
+        const chart = createChart(chartContainerRef.current);
         chart.applyOptions({
             layout: {
                 background: {
@@ -74,23 +47,114 @@ const LightWeightChart = ({ theme }) => {
             }
         });
 
-        // const series = chart.addLineSeries();
-        const series = chart.addCandlestickSeries();
-        series.setData(data);
+        const candleSeries = chart.addCandlestickSeries();
+        const data = [
+            { time: "2018-12-22", open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
+            { time: "2018-12-23", open: 45.12, high: 53.90, low: 45.12, close: 48.09 },
+            { time: "2018-12-24", open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
+            { time: "2018-12-25", open: 68.26, high: 68.26, low: 59.04, close: 60.50 },
+            { time: "2018-12-26", open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
+            { time: "2018-12-27", open: 91.04, high: 121.40, low: 82.70, close: 111.40 },
+            { time: "2018-12-28", open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
+            { time: "2018-12-29", open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
+            { time: "2018-12-30", open: 106.33, high: 110.20, low: 90.39, close: 98.10 },
+            { time: "2018-12-31", open: 109.87, high: 114.69, low: 85.66, close: 111.26 }
+        ];
+
+        candleSeries.setData(data);
+
+        let lastClose = data[data.length - 1].close;
+        let lastIndex = data.length - 1;
+        let targetIndex = lastIndex + 105 + Math.round(Math.random() + 30);
+        let targetPrice = getRandomPrice();
+        let currentIndex = lastIndex + 1;
+        let currentBusinessDay = { day: 29, month: 5, year: 2019 };
+        let ticksInCurrentBar = 0;
+        let currentBar = {
+            open: null,
+            high: null,
+            low: null,
+            close: null,
+            time: currentBusinessDay
+        };
+
+        function mergeTickToBar(price) {
+            if (currentBar.open === null) {
+                currentBar.open = price;
+                currentBar.high = price;
+                currentBar.low = price;
+                currentBar.close = price;
+            } else {
+                currentBar.close = price;
+                currentBar.high = Math.max(currentBar.high, price);
+                currentBar.low = Math.min(currentBar.low, price);
+            }
+            candleSeries.update(currentBar);
+        };
+
+        function reset() {
+            candleSeries.setData(data);
+            lastClose = data[data.length - 1].close;
+            lastIndex = data.length - 1;
+            targetIndex = lastIndex + 5 + Math.round(Math.random() + 30);
+            targetPrice = getRandomPrice();
+            currentIndex = lastIndex + 1;
+            currentBusinessDay = { day: 29, month: 5, year: 2019 };
+            ticksInCurrentBar = 0;
+        };
+
+        function getRandomPrice() {
+            return 10 + Math.round(Math.random() * 10000) / 100;
+        };
 
         function nextBusinessDay(time) {
-            var date = new Date();
-            date.setUTCFullYear(time.year);
-            date.setUTCMonth(time.month - 1);
-            date.setUTCDate(time.day + 1);
-            date.setUTCHours(0, 0, 0, 0);
+            const d = new Date();
+            d.setUTCFullYear(time.year);
+            d.setUTCMonth(time.month - 1);
+            d.setUTCDate(time.day + 1);
+            d.setUTCHours(0, 0, 0, 0);
 
             return {
-                year: date.getUTCFullYear(),
-                month: date.getUTCMonth() + 1,
-                day: date.getUTCDate()
+                year: d.getUTCFullYear(),
+                month: d.getUTCMonth() + 1,
+                day: d.getUTCDate()
             };
         };
+
+        const interval = setInterval(() => {
+            const deltaY = targetPrice - lastClose;
+            const deltaX = targetIndex - lastIndex;
+            const angle = deltaY / deltaX;
+            const basePrice = lastClose + (currentIndex - lastIndex) * angle;
+            const noise = 0.1 - Math.random() * 0.1 + 1.0;
+            const noisedPrice = basePrice * noise;
+            mergeTickToBar(noisedPrice);
+
+            if (++ticksInCurrentBar === 5) {
+                currentIndex++;
+                currentBusinessDay = nextBusinessDay(currentBusinessDay);
+                currentBar = {
+                    open: null,
+                    high: null,
+                    low: null,
+                    close: null,
+                    time: currentBusinessDay
+                };
+
+                ticksInCurrentBar = 0;
+                if (currentIndex === 5000) {
+                    reset();
+                    return;
+                }
+
+                if (currentIndex === targetIndex) {
+                    lastClose = noisedPrice;
+                    lastIndex = currentIndex;
+                    targetIndex = lastIndex + 5 + Math.round(Math.random() + 30);
+                    targetPrice = getRandomPrice();
+                }
+            }
+        }, 200);
 
         const symbolName = "BINANCE:BTCUSDT";
         legend.style = `position: absolute; left: 12px; top: 12px; z-index: 1; font-size: 14px; line-height: 18px; font-weight: 300;`;
@@ -143,22 +207,14 @@ const LightWeightChart = ({ theme }) => {
                     <div style="font-size: 12px; margin: 4px 0px;">${date}</div>
                 `;
             }
-        }
-
-        // const setTooltipHtml = (name, date, price) => {
-        //     legend.innerHTML = `
-        //         <div style="font-size: 24px; margin: 4px 0px;">${name}</div>
-        //         <div style="font-size: 22px; margin: 4px 0px;">${price}</div>
-        //         <div>${date}</div>
-        //     `;
-        // };
+        };
 
         const updateLegend = param => {
             const validCrosshairPoint = !(
                 param === undefined || param.time === undefined || param.point.x < 0 || param.point.y < 0
             );
 
-            const bar = validCrosshairPoint ? param.seriesData.get(series) : getLastBar(series);
+            const bar = validCrosshairPoint ? param.seriesData.get(candleSeries) : getLastBar(candleSeries);
             const time = bar.time;
             const open = bar.open;
             const high = bar.high;
@@ -176,28 +232,8 @@ const LightWeightChart = ({ theme }) => {
         updateLegend(undefined);
         chart.timeScale().fitContent();
 
-        setInterval(() => {
-            const lastItemTime = data[data.length - 1].time;
-            const nextTime = nextBusinessDay(lastItemTime);
-
-            // const newCandle = {
-            //     time: nextTime,
-            //     value: 58 + Math.random() * 2
-            // };
-
-            const newCandle = {
-                time: nextTime,
-                high: 50 + Math.random() * 2,
-                open: 50 + Math.random() * 2,
-                low: 40 + Math.random() * 2,
-                close: 40 + Math.random() * 20
-            };
-
-            series.update(newCandle);
-            data.push(newCandle);
-        }, 1000);
-
         return () => {
+            clearInterval(interval);
             chart.remove();
         };
     }, [theme, legend]);
