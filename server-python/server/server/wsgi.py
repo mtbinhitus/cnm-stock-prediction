0e887fd1d0eb53cf4f6181b58fa1782110074f3a
Binh Mai <mtbinhitus@gmail.com>
2023-08-30 05:21:58 +0700

    update add registry when start the backend

--- server-python/server/server/wsgi.py
+++ server-python/server/server/wsgi.py
@@ -18,19 +18,41 @@ application = get_wsgi_application()
 # ML registry
 from ml.registry import MLRegistry
 from ml.btc_sticker.btc_lstm import BTCPredictionUsingLSTM
+from ml.btc_sticker.btc_rnn import BTCPredictionUsingRNN
+from ml.btc_sticker.btc_xgboost import BTCPredictionUsingXGBOOST
 try:
     # create ML registry
     registry = MLRegistry()
     # BTCLSTM
-    rf = BTCPredictionUsingLSTM()
+    lstm = BTCPredictionUsingLSTM()
     # add to ML registry
+    print("Import Model")
     registry.add_algorithm(endpoint_name="btcsticker",
-                            algorithm_object=rf,
-                            algorithm_name="btcusdt_lstm_close_ma_poc",
+                            algorithm_object=lstm,
+                            algorithm_name="btcusdt_lstm",
                             algorithm_status="production",
                             algorithm_version="0.0.1",
                             owner="Binh Mai",
-                            algorithm_description="LSTM using ROC, MA, SD to predict 1h candle price of BTCUSDT.")
+                            algorithm_description="LSTM model to predict close price of BTCUSDT.")
+    
+    # BTCRNN
+    rnn = BTCPredictionUsingRNN()
+    registry.add_algorithm(endpoint_name="btcsticker",
+                            algorithm_object=rnn,
+                            algorithm_name="btcusdt_rnn",
+                            algorithm_status="production",
+                            algorithm_version="0.0.1",
+                            owner="Binh Mai",
+                            algorithm_description="RNN model to predict close price of BTCUSDT.")
+    
+    # BTCXGBoost
+    xgboost = BTCPredictionUsingXGBOOST()
+    registry.add_algorithm(endpoint_name="btcsticker",
+                            algorithm_object=xgboost,
+                            algorithm_name="btcusdt_xgboost",
+                            algorithm_status="production",
+                            algorithm_version="0.0.1",
+                            owner="Binh Mai",
+                            algorithm_description="XGBoost model to predict close price of BTCUSDT.")
 except Exception as e:
     print("Exception while loading the algorithms to the registry,", str(e))
-
