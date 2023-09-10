ea01cf73e0f5f05585db98a56252e14b999eba85
longproxd <longproxd1@gmail.com>
2023-09-10 21:17:55 +0700

    bug fix

--- client-react/src/components/LightWeightChart.js
+++ client-react/src/components/LightWeightChart.js
@@ -132,19 +132,21 @@ const LightWeightChart = ({ theme, smaCount, data, prediction, crypto, model, in
             }
         });
 
-        candleStickSeriesRef.current = chartRef.current.addCandlestickSeries();
+        candleStickSeriesRef.current = chartRef.current.addCandlestickSeries({
+            title: "Current price"
+        });
+
+        predictionLineSeriesRef.current = chartRef.current.addLineSeries({
+            title: "Predicted price",
+            color: ThemeColors.predictionColor,
+            lineWidth: 1
+        });
 
         legendRef.current = document.createElement("div");
         legendRef.current.className = "legend";
         legendRef.current.style = `position: absolute; left: 0px; top: 0px; z-index: 1;`;
         legendRef.current.style.color = textColor;
         chartContainerRef.current.appendChild(legendRef.current);
-
-        predictionLineSeriesRef.current = chartRef.current.addLineSeries({
-            title: "Close Prediction",
-            color: ThemeColors.predictionColor,
-            lineWidth: 1
-        });
 
         return () => {
             legendRef.current.remove();
@@ -153,13 +155,13 @@ const LightWeightChart = ({ theme, smaCount, data, prediction, crypto, model, in
     }, []);
 
     useEffect(() => {
-        chartRef.current.removeSeries(candleStickSeriesRef.current);
-        candleStickSeriesRef.current = null;
-        candleStickSeriesRef.current = chartRef.current.addCandlestickSeries();
+        candleStickSeriesRef.current.setData([]);
     }, [data]);
 
     useEffect(() => {
-        candleStickSeriesRef.current.setData(data);
+        if (data.some(item => item.time !== undefined)) {
+            candleStickSeriesRef.current.setData(data);
+        }
     }, [data]);
 
     useEffect(() => {
@@ -178,6 +180,28 @@ const LightWeightChart = ({ theme, smaCount, data, prediction, crypto, model, in
         }
         // eslint-disable-next-line
     }, [prediction]);
+
+    useEffect(() => {
+        if (sma5LineSeriesRef.current) {
+            chartRef.current.removeSeries(sma5LineSeriesRef.current);
+            sma5LineSeriesRef.current = null;
+        }
+
+        if (sma10LineSeriesRef.current) {
+            chartRef.current.removeSeries(sma10LineSeriesRef.current);
+            sma10LineSeriesRef.current = null;
+        }
+
+        if (sma20LineSeriesRef.current) {
+            chartRef.current.removeSeries(sma20LineSeriesRef.current);
+            sma20LineSeriesRef.current = null;
+        }
+
+        if (sma40LineSeriesRef.current) {
+            chartRef.current.removeSeries(sma40LineSeriesRef.current);
+            sma40LineSeriesRef.current = null;
+        }
+    }, [data]);
 
     useEffect(() => {
         const color = theme === "dark" ? ThemeColors.darkBlue : ThemeColors.white;
@@ -234,23 +258,25 @@ const LightWeightChart = ({ theme, smaCount, data, prediction, crypto, model, in
 
                 const bar = validCrosshairPoint ? param.seriesData.get(candleStickSeriesRef.current) : lastIndex;
 
-                const time = new Date(0);
-                time.setUTCSeconds(bar.time);
-                const day = time.getUTCDate();
-                const month = time.toLocaleString("default", { month: "long" });
-                const year = time.getUTCFullYear();
-                const hours = time.getUTCHours().toString().padStart(2, "0");
-                const minutes = time.getUTCMinutes().toString().padStart(2, "0");
+                if (bar !== undefined) {
+                    const time = new Date(0);
+                    time.setUTCSeconds(bar.time);
+                    const day = time.getUTCDate();
+                    const month = time.toLocaleString("default", { month: "long" });
+                    const year = time.getUTCFullYear();
+                    const hours = time.getUTCHours().toString().padStart(2, "0");
+                    const minutes = time.getUTCMinutes().toString().padStart(2, "0");
 
-                const open = bar.open.toFixed(2);
-                const high = bar.high.toFixed(2);
-                const low = bar.low.toFixed(2);
-                const close = bar.close.toFixed(2);
+                    const open = bar.open.toFixed(2);
+                    const high = bar.high.toFixed(2);
+                    const low = bar.low.toFixed(2);
+                    const close = bar.close.toFixed(2);
 
-                const symbolName = "BINANCE:BTCUSDT";
-                const formattedTime = `${day} ${month} ${year} ${hours}:${minutes}`;
+                    const symbolName = "BINANCE:BTCUSDT";
+                    const formattedTime = `${day} ${month} ${year} ${hours}:${minutes}`;
 
-                setTooltipHtml(symbolName, formattedTime, open, high, low, close);
+                    setTooltipHtml(symbolName, formattedTime, open, high, low, close);
+                }
             };
 
             chartRef.current.subscribeCrosshairMove(updateLegend);
@@ -263,7 +289,7 @@ const LightWeightChart = ({ theme, smaCount, data, prediction, crypto, model, in
             if (smaCount.includes(period)) {
                 if (!seriesRef.current) {
                     seriesRef.current = chartRef.current.addLineSeries({
-                        title: "SMA(" + period + ")",
+                        title: "SMA " + period + "",
                         color: color,
                         lineWidth: 1
                     });
