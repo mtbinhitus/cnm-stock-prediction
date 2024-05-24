f6769847cb18917563739a69c287231366c4d779
longproxd <longproxd1@gmail.com>
2024-05-24 15:24:35 +0700

    fix env

--- server-python/server/server/settings.py
+++ server-python/server/server/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 import os
 import dotenv
 
-dotenv.load_dotenv()
+BASE_DIR_env = os.path.dirname(os.path.abspath(__file__))
+dotenv_path = os.path.join(BASE_DIR_env, "../.env")
+dotenv.load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
